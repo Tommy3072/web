86a40622bf90e8d90e15c02b637449ec8da6ebe9
Tommy3072 <89037957+Tommy3072@users.noreply.github.com>
2024-08-13 14:27:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -6,27 +6,35 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    message = ""
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            message = "No se ha seleccionado ningún archivo"
-        else:
-            file = request.files['file']
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
 
-            if file.filename == '':
-                message = "No se ha seleccionado ningún archivo"
-            elif file and file.filename.endswith('.pdf'):
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-                file.save(filepath)
-                message = f"Archivo subido exitosamente: {file.filename}"
-            else:
-                message = "El archivo no es un PDF válido"
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No se ha seleccionado ningún archivo"
     
-    return render_template('upload.html', message=message)
+    file = request.files['file']
+
+    if file.filename == '':
+        return "No se ha seleccionado ningún archivo"
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+    return redirect(url_for('list_files'))
+
+@app.route('/files')
+def list_files():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('list_files.html', files=files)
+
+@app.route('/files/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
