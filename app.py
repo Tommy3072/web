c03ae4f9198538c9696988b94bca624270bf7a25
Tommy3072 <89037957+Tommy3072@users.noreply.github.com>
2024-08-13 14:36:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -12,30 +12,20 @@ def upload_form():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return "No se ha seleccionado ningún archivo"
-        
-        file = request.files['file']
+    if 'file' not in request.files:
+        return render_template('upload.html', message="No se ha seleccionado ningún archivo.")
+    
+    file = request.files['file']
 
-        if file.filename == '':
-            return "No se ha seleccionado ningún archivo"
+    if file.filename == '':
+        return render_template('upload.html', message="No se ha seleccionado ningún archivo.")
 
-        if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filepath)
-            return redirect(url_for('list_files'))
-
-@app.route('/files')
-def list_files():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('list_files.html', files=files)
-
-@app.route('/files/<filename>')
-def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+    return render_template('upload.html', message=f"Archivo subido exitosamente: {file.filename}")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
